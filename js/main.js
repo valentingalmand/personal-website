b3c6ce5dbacffc87fa515fd777a29b583f879ba0
valentingalmand <Valentin@MacBook-Pro-de-Valentin.local>
2015-07-16 11:59:45 +0200

    i think we are good

--- js/main.js
+++ js/main.js
@@ -16,3 +16,24 @@ $(".menu--link").click(function(event) {
         hashTagActive = this.hash;
     }
 });
+
+$(document).ready(function(){
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 300) {
+            $('.scrollToTop').removeClass('scrollToTop--is-gone');
+			$('.scrollToTop').fadeIn();
+			$('.scrollToTop').addClass('scrollToTop--is-active');
+		} else if (!$('.scrollToTop').hasClass('scrollToTop--is-gone') && $('.scrollToTop').hasClass('scrollToTop--is-active')) {
+			// $('.scrollToTop').fadeOut();
+            $('.scrollToTop').addClass('scrollToTop--is-gone');
+            $('.scrollToTop').removeClass('scrollToTop--is-active');
+		}
+	});
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+        $(this).addClass('scrollToTop--is-gone');
+	});
+});
