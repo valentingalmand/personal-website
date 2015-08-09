4f72f7c99d7e775722a8b4afb8704867803c5a71
valentingalmand <Valentin@MacBook-Pro-de-Valentin.local>
2015-08-10 01:08:29 +0200

    V3

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@ $(".menu--link").click(function(event) {
         //calculate destination place
         var dest = 0;
         if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
-            dest = $(document).height() - $(window).height();
+            dest = $(document).height() + 50 - $(window).height();
         } else {
             dest = $(this.hash).offset().top;
         }
         //go to destination
         $('html,body').animate({
-            scrollTop: dest
+            scrollTop: dest - 50
         }, 500, 'swing');
         hashTagActive = this.hash;
     }
